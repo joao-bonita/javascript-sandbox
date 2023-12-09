f6bea1aad39cec8a61f1adbbfe04655fb8d734df
Joao Bonita <jmbploureiro@gmail.com>
2023-12-09 11:43:00 +0000

    Clean up JSDOM as best as we can and refactor code such that we don't have global variables.

--- javascript-sandbox-start/08-shopping-list-project/shopping-list/script.js
+++ javascript-sandbox-start/08-shopping-list-project/shopping-list/script.js
@@ -1,12 +1,17 @@
-const itemForm = document.getElementById("item-form");
 
-function addItemToList(event) {
-  event.preventDefault();
-  const newItem = document.getElementById("item-input").value;
-  const itemElement = document.createElement("li");
-  itemElement.textContent = newItem;
-  const itemsList = document.getElementById("item-list");
-  itemsList.prepend(itemElement);
+function runMyJavascript() {
+  const itemForm = document.getElementById("item-form");
+
+  function addItemToList(event) {
+    event.preventDefault();
+    const newItem = document.getElementById("item-input").value;
+    const itemElement = document.createElement("li");
+    itemElement.textContent = newItem;
+    const itemsList = document.getElementById("item-list");
+    itemsList.prepend(itemElement);
+  }
+
+  itemForm.addEventListener("submit", addItemToList);
 }
 
-itemForm.addEventListener("submit", addItemToList);
+runMyJavascript();
--- javascript-sandbox-start/08-shopping-list-project/shopping-list/script.test.js
+++ javascript-sandbox-start/08-shopping-list-project/shopping-list/script.test.js
@@ -3,15 +3,87 @@
  */
 
 import "@testing-library/jest-dom";
-import {screen} from "@testing-library/dom";
+import {screen, within} from "@testing-library/dom";
 import {userEvent} from "@testing-library/user-event";
 import {promises as fs} from "fs";
 
-test("Should add an item to the top of the list when using the 'Add Item' button", async () => {
+const sideEffects = {
+  document: {
+    addEventListener: {
+      fn: document.addEventListener,
+      refs: [],
+    },
+    keys: Object.keys(document),
+  },
+  window: {
+    addEventListener: {
+      fn: window.addEventListener,
+      refs: [],
+    },
+    keys: Object.keys(window),
+  },
+};
+
+// Lifecycle Hooks
+// -----------------------------------------------------------------------------
+beforeAll(async () => {
+  // Spy addEventListener
+  ['document', 'window'].forEach(obj => {
+    const fn = sideEffects[obj].addEventListener.fn;
+    const refs = sideEffects[obj].addEventListener.refs;
+
+    function addEventListenerSpy(type, listener, options) {
+      // Store listener reference so it can be removed during reset
+      refs.push({ type, listener, options });
+      // Call original window.addEventListener
+      fn(type, listener, options);
+    }
+
+    // Add to default key array to prevent removal during reset
+    sideEffects[obj].keys.push('addEventListener');
+
+    // Replace addEventListener with mock
+    global[obj].addEventListener = addEventListenerSpy;
+  });
+});
+
+// Reset JSDOM. This attempts to remove side effects from tests, however it does
+// not reset all changes made to globals like the window and document
+// objects. Tests requiring a full JSDOM reset should be stored in separate
+// files, which is the only way to do a complete JSDOM reset with Jest.
+beforeEach(async () => {
+  const rootElm = document.documentElement;
+
+  // Remove global listeners and keys
+  ['document', 'window'].forEach(obj => {
+    const refs = sideEffects[obj].addEventListener.refs;
+
+    // Listeners
+    while (refs.length) {
+      const { type, listener, options } = refs.pop();
+      global[obj].removeEventListener(type, listener, options);
+    }
+
+    // Keys
+    Object.keys(global[obj])
+    .filter(key => !sideEffects[obj].keys.includes(key))
+    .forEach(key => {
+      const globalObject = global[obj];
+      if (globalObject[key] instanceof Function) {
+        globalObject[key] = null;
+      } else {
+        delete globalObject[key];
+      }
+    });
+  });
+
   await loadHtmlAndScript(
     "./javascript-sandbox-start/08-shopping-list-project/shopping-list/index.html",
     "./javascript-sandbox-start/08-shopping-list-project/shopping-list/script.js");
 
+});
+
+test("Should add an item to the top of the list when using the 'Add Item' button", async () => {
   const itemInput = screen.getByRole("textbox", {name: "Enter Item"});
   const user = userEvent.setup();
   await user.click(itemInput);
@@ -24,8 +96,20 @@ test("Should add an item to the top of the list when using the 'Add Item' button
   await user.click(addItemButton);
 
   const itemsList = screen.getByRole("list");
-  expect(itemsList.firstElementChild).toHaveTextContent("Cheese");
+  expect(itemsList.children.item(0)).toHaveTextContent("Cheese");
   expect(itemsList.children.item(1)).toHaveTextContent("Eggs");
+});
+
+test("Should add a 'remove item' button to a new item", async () => {
+  const itemInput = screen.getByRole("textbox", {name: "Enter Item"});
+  const user = userEvent.setup();
+  await user.click(itemInput);
+  await user.type(itemInput, "Eggs");
+  const addItemButton = screen.getByRole("button", {name: "Add Item"});
+  await user.click(addItemButton);
+
+  const newListItem = screen.getByRole("list").firstElementChild;
+  within(newListItem).getByRole("button");
 });
 
 async function loadHtmlAndScript(htmlFilepath, scriptFilepath) {
@@ -45,5 +129,5 @@ async function addScriptToBody(scriptFilepath) {
   const scriptContents = await fs.readFile(scriptFilepath, "utf8");
   const scriptEl = document.createElement("script");
   scriptEl.textContent = scriptContents;
-  document.body.appendChild(scriptEl);
+  document.body.replaceChild(scriptEl, document.body.querySelector(("script")));
 }
