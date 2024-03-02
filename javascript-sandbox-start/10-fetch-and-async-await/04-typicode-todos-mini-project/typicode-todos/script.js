f4b3ee49d6cad08b0b33117bab18ee467bde9e29
Joao Bonita <jmbploureiro@gmail.com>
2024-03-02 12:32:22 +0000

    Typicode: Add todos from server when page loads.

---
+++ javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -0,0 +1,17 @@
+function runMyJavaScript() {
+  const todoList = document.getElementById("todo-list");
+
+  const apiUrl = new URL("todos", "https://jsonplaceholder.typicode.com");
+  apiUrl.searchParams.append("_limit", "5");
+  fetch(apiUrl.toString())
+    .then(response => response.json())
+    .then(todoData => {
+      todoData.forEach(todo => {
+          const todoElement = document.createElement("div");
+          todoElement.textContent = todo.title;
+          todoList.appendChild(todoElement);
+      })
+    });
+}
+
+runMyJavaScript();
---
+++ javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.test.js
@@ -0,0 +1,145 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import "@testing-library/jest-dom";
+import {screen, waitFor} from "@testing-library/dom";
+import {loadHtmlAndScript} from "../../../testing/MoreTesting";
+import "whatwg-fetch";
+
+const sideEffects = {
+  document: {
+    addEventListener: {
+      fn: document.addEventListener,
+      refs: [],
+    },
+    keys: Object.keys(document),
+  },
+  window: {
+    addEventListener: {
+      fn: window.addEventListener,
+      refs: [],
+    },
+    keys: Object.keys(window),
+  },
+};
+
+// Lifecycle Hooks
+// -----------------------------------------------------------------------------
+beforeAll(async () => {
+  // Spy addEventListener
+  ['document', 'window'].forEach(obj => {
+    const fn = sideEffects[obj].addEventListener.fn;
+    const refs = sideEffects[obj].addEventListener.refs;
+
+    function addEventListenerSpy(type, listener, options) {
+      // Store listener reference so it can be removed during reset
+      refs.push({ type, listener, options });
+      // Call original window.addEventListener
+      fn(type, listener, options);
+    }
+
+    // Add to default key array to prevent removal during reset
+    sideEffects[obj].keys.push('addEventListener');
+
+    // Replace addEventListener with mock
+    global[obj].addEventListener = addEventListenerSpy;
+  });
+});
+
+// Reset JSDOM. This attempts to remove side effects from tests, however it does
+// not reset all changes made to globals like the window and document
+// objects. Tests requiring a full JSDOM reset should be stored in separate
+// files, which is the only way to do a complete JSDOM reset with Jest.
+beforeEach(async () => {
+  // Remove global listeners and keys
+  ['document', 'window'].forEach(obj => {
+    const refs = sideEffects[obj].addEventListener.refs;
+
+    // Listeners
+    while (refs.length) {
+      const { type, listener, options } = refs.pop();
+      global[obj].removeEventListener(type, listener, options);
+    }
+
+    // Keys
+    Object.keys(global[obj])
+    .filter(key => !sideEffects[obj].keys.includes(key))
+    .forEach(key => {
+      const globalObject = global[obj];
+      if (globalObject[key] instanceof Function) {
+        globalObject[key] = null;
+      } else {
+        delete globalObject[key];
+      }
+    });
+
+  });
+
+  jest.restoreAllMocks();
+});
+
+describe("Initial page load", () => {
+  test("Should display 5 of the existing todos returned by the Web API", async () => {
+    jest.spyOn(global, "fetch").mockResolvedValue(new Response(
+      JSON.stringify(getJsonDataFor5Todos()),
+    ));
+
+    await loadPage();
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos?_limit=5");
+
+    const todoList = screen.getByTestId("todo-list");
+    await waitFor(() => expect(
+      todoList.querySelectorAll("div").length).toBe(5)
+    );
+    expect(todoList).toHaveTextContent("delectus aut autem");
+    expect(todoList).toHaveTextContent("quis ut nam facilis et officia qui");
+    expect(todoList).toHaveTextContent("fugiat veniam minus");
+    expect(todoList).toHaveTextContent("et porro tempora");
+    expect(todoList).toHaveTextContent("laboriosam mollitia et enim quasi adipisci quia provident illum");
+  });
+});
+
+async function loadPage() {
+  await loadHtmlAndScript(
+    "./javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/index.html",
+    "./javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js"
+  );
+}
+
+function getJsonDataFor5Todos() {
+  return [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    }
+  ];
+}
